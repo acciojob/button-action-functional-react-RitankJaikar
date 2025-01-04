3a500989ddaa4c553b0d5c0f24cf3781daaa06b1
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-04 16:04:55 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import React, { useState } from "react";
 
 function App() {
+  // State to control the display of the paragraph
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  // Function to handle button click
+  const handleClick = () => {
+    setShowParagraph(true); // Update state to display the paragraph
+  };
+
   return (
-    <div id="main">
-      // Do not alter the main div
+    <div>
+      {/* Button to trigger paragraph display */}
+      <button id="click" onClick={handleClick}>
+        Click Me
+      </button>
+
+      {/* Conditionally render the paragraph */}
+      {showParagraph && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy.
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
